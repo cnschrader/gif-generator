de1a8f25957534378ad5d6485f498ef8e293b518
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-03-24 17:45:46 -0500

    array added and some divs added

---
+++ assets/javascript/gif.js
@@ -0,0 +1,2 @@
+var topics = ["ducks", "cats", "dogs", "mice", "cows", "hampsters", "goat", "bird", "alpaca", "llama", "anteater", "monkeys", "owls", "dolphins", "unicorns", "rabbits", "chinchilla", "badger", "otter", "penguin", "lizard"];
+

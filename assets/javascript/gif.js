3bfb22b1a614b7ffd84b158ce0739f10b2589a6a
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-03-25 19:32:39 -0500

    buttons generated, but nothing works

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,2 +1,31 @@
 var topics = ["ducks", "cats", "dogs", "mice", "cows", "hampsters", "goat", "bird", "alpaca", "llama", "anteater", "monkeys", "owls", "dolphins", "unicorns", "rabbits", "chinchilla", "badger", "otter", "penguin", "lizard"];
+var gifs;
+var queryURL;
+function displayGifs(){
+    
+     gifs = $(this).attr("data-name");
+     queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "api_key=8b0LeUGaPodQPOpR08scJuSmwZULUz0z&q=&limit=10&offset=0&rating=G&lang=en"
 
+     .ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+          console.log(response);
+         results = response.data;
+      });
+};
+
+function generateButtons(){
+   
+   
+    for(var i = 0; i < topics.length; i++){
+        var newButton = $("<button>");
+        newButton.addClass(".gifs");
+        newButton.attr("data-name", topics[i]);
+        newButton.text(topics[i]);
+        $("#buttonDisplay").append(newButton);
+    }
+};
+
+generateButtons();

834e29040d07c914a59dd788aff21b62468b5c1d
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-03-30 16:34:36 -0500

    spacing and jumbotron

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,22 +1,7 @@
 var topics = ["ducks", "cats", "dogs", "mice", "cows", "hampsters", "goat", "bird", "alpaca", "llama", "anteater", "monkeys", "owls", "dolphins", "unicorns", "rabbits", "chinchilla", "badger", "otter", "penguin", "lizard"];
 
 var gifDisplay = $("#gifDisplay");
-// var gifs;
-// var queryURL;
-// function displayGifs(){
 
-//      gifs = $(this).attr("data-name");
-//      queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "api_key=8b0LeUGaPodQPOpR08scJuSmwZULUz0z&limit=10&offset=0&rating=G&lang=en"
-
-//      .ajax({
-//         url: queryURL,
-//         method: "GET"
-//       })
-//       .then(function(response) {
-//           console.log(response);
-//          results = response.data;
-//       });
-// };
 
 function generateButtons() {
     
@@ -54,8 +39,9 @@ $(document).on("click", ".gifs", function () {
         
         for (var i = 0; i < results.length; i++) {
             gifDiv = $("<div>");
+            gifDiv.addClass("gifmargin")
             var rating = results[i].rating;
-            ratingDisplay = $("<p>").text("rating:" + rating);
+            ratingDisplay = $("<p>").text("rating:" + " " + rating);
             gifImage = $("<img>");
             gifImage.attr("src", results[i].images.fixed_height_still.url);
             gifImage.attr("data-still", results[i].images.fixed_height_still.url);
@@ -69,6 +55,47 @@ $(document).on("click", ".gifs", function () {
         
         
     });
+    
+    
+    $("#submitButton").on("click", function(){
+        
+        event.preventDefault();
+        
+        userInput = $("#inlineFormInputName2").val().trim();
+        
+        topics.push(userInput);
+        
+        console.log(topics);
+        
+        generateButtons();
+    });
+    
+    
+    
+    $(document).on("click", ".gif-animate", function () {
+        
+        var state = $(this).attr("data-state");
+        
+        console.log(state);
+        
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+        
+    });
+    
+    
+    
+    
+    
+    
+    
     //  for(var i = 0; i < results.length; i++){
         
         //  var rating = results[i].rating;
@@ -96,43 +123,3 @@ $(document).on("click", ".gifs", function () {
             //  $("#gifDisplay").prepend(gifDiv);
             // }
         });
-        
-        
-        $("#submitButton").on("click", function(){
-        
-            event.preventDefault();
-        
-            userInput = $("#inlineFormInputName2").val().trim();
-          
-            topics.push(userInput);
-        
-            console.log(topics);
-        
-            generateButtons();
-        });
-
-
-
-$(document).on("click", ".gif-animate", function () {
-
-    var state = $(this).attr("data-state");
-
-    console.log(state);
-
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    }
-
-    else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-
-});
-
-
-
-
-
-
